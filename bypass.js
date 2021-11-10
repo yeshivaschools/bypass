3f9fa60fec552dc885c05b8b10d55af1be6b5eed
aroary <meanchemshkedi@gmail.com>
2021-11-10 09:56:41 -0500

    alert notification

--- bypass.js
+++ bypass.js
@@ -1,3 +1,3 @@
-document.addEventListener("load", () => {
-    alert("Bypassed");
-});
+window.onload = () => {
+    alert("Bypass your school internet restrictions with this remote server.\nByn\n\taroary#0500\n\taroary.exe\n\taroary");
+};
