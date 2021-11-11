6bc1c04bfdc5e7a1dc6188745479fb3798ac3d4b
aroary <71286008+aroary@users.noreply.github.com>
2021-11-10 20:28:47 -0500

    security message

--- bypass.js
+++ bypass.js
@@ -1,3 +1,10 @@
+const licence = `This is free and unencumbered software released into the public domain.
+\n\tAnyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+\n\tIn jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
+\n\tTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+\nFor more information, please refer to <https://unlicense.org>`
+
 window.onload = () => {
     alert("Bypass your school internet restrictions with this remote server.\n\taroary#0500\t(discord)\n\taroary.exe\t(instagram)\n\taroary\t(github)");
+    alert("This can be potentially insecure, it is not recommended to enter personal or private information into a website here.\nIf you need to log into any sort of account it is recommended to use 2fa\n" + licence);
 };
